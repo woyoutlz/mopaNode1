268ba4f31ff0030a32d57df86197344c9642ab40
woyoutlz <yundkyy@gmail.com>
2014-07-01 10:46:45 +0800

    Update node.js

--- node.js
+++ node.js
@@ -4,5 +4,5 @@ var http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World From MoPaaS\n');
+  res.end('Hello World From 11S\n');
 }).listen(port, host);
